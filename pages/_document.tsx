7f164b5ba7910b10c98c7c1f483633d7dd766d43
benguyucel <yucelbengu8@gmail.com>
2022-11-14 15:42:46 +0300

    asd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,700&display=swap" rel="stylesheet" />
             </Head>
             <body>
